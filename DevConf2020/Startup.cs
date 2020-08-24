92f4ecde5019582dac92167ac6fb571508adcd26
Hosenbocus,Wasiim <wasiim.hosenbocus@Ceridian.com>
2020-08-24 17:19:20 +0400

    Added Application Insight

--- DevConf2020/Startup.cs
+++ DevConf2020/Startup.cs
@@ -29,6 +29,7 @@ namespace DevConf2020
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
